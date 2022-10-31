40c337f22b792f7ee8d7ac124b9e0286ea34bb24
Sele3 <STAN266@e.ntu.edu.sg>
2022-10-31 22:38:44 +0800

    updated menu options

--- src/admin/AdminController.java
+++ src/admin/AdminController.java
@@ -8,21 +8,6 @@ import main.MOBLIMA;
 public class AdminController {
 	private String password = "admin";
 	
-	private boolean login() {
-		System.out.print("Enter password: ");
-		boolean isValid = StringHandler.readString().equals(password);
-		
-		if (!isValid) 
-			System.out.println("Wrong password. Exiting.");
-		return isValid;
-	}
-	
-	
-	private void changePassword() {
-		System.out.print("Enter new password: ");
-		password = StringHandler.readString();
-	}
-	
 	private void displayAdminMenu() {
 		System.out.println(
 				"======================= Admin Menu =======================\n" + 
@@ -36,8 +21,9 @@ public class AdminController {
 	}
 	
 	public void run() {
-		if (!login())
-			return;
+//		Comment out for convenience, uncomment when done
+//		if (!login())
+//			return;
 		
 		boolean done = false;
 		do {
@@ -45,9 +31,11 @@ public class AdminController {
 			
 			switch (IntegerHandler.readInt(1, 6)) {
 			case 1:
+				configureMovies();
 				break;
 
 			case 2:
+				configureShowtimes();
 				break;
 				
 			case 3:
@@ -55,6 +43,8 @@ public class AdminController {
 				break;
 				
 			case 4:
+				System.out.print("Sort by ticket sales? (Y/N) ");
+				MOBLIMA.movieController.listTopFive(StringHandler.readString("Y", "N").equals("Y"));
 				break;
 				
 			case 5:
@@ -66,6 +56,82 @@ public class AdminController {
 				done = true;
 			}
 			
+		} while (!done);
+	}
+	
+	// Simple login method
+	private boolean login() {
+		System.out.print("Enter password: ");
+		boolean isValid = StringHandler.readString().equals(password);
+		
+		if (!isValid) 
+			System.out.println("Wrong password. Exiting.");
+		return isValid;
+	}
+	// Change password
+	private void changePassword() {
+		System.out.print("Enter new password: ");
+		password = StringHandler.readString();
+	}
+	
+	
+	private void configureMovies() {
+		boolean done = false;
+		do {
+			System.out.println("Configuring Movie Listing...\n" +
+					   "1) Create new movie listing\n" + 
+					   "2) Update movie listing\n" +
+					   "3) Remove movie listing\n" +
+					   "4) Back");
+			System.out.print("Please select an option: ");
+			
+			switch (IntegerHandler.readInt(1, 4)) {
+			case 1:
+				MOBLIMA.movieController.createMovie();
+				break;
+
+			case 2:
+				MOBLIMA.movieController.updateMovie();
+				break;
+				
+			case 3:
+				MOBLIMA.movieController.deleteMovie();
+				break;
+				
+			case 4:
+				done = true;
+				break;
+			}
+		} while (!done);
+	}
+	
+	private void configureShowtimes() {
+		boolean done = false;
+		do {
+			System.out.println("Configuring Cinema Showtimes...\n" +
+					   "1) Create new showtime\n" + 
+					   "2) Update existing showtime\n" +
+					   "3) Remove existing showtime\n" +
+					   "4) Back");
+			System.out.print("Please select an option: ");
+			
+			switch (IntegerHandler.readInt(1, 4)) {
+			case 1:
+				MOBLIMA.showtimeController.createShowtime();
+				break;
+
+			case 2:
+				MOBLIMA.showtimeController.updateShowtime();
+				break;
+				
+			case 3:
+				MOBLIMA.showtimeController.deleteShowtime();
+				break;
+				
+			case 4:
+				done = true;
+				break;
+			}
 		} while (!done);
 	}
 }
--- src/customer/CustomerController.java
+++ src/customer/CustomerController.java
@@ -1,17 +1,21 @@
 package customer;
 
 import input.IntegerHandler;
+import input.StringHandler;
+import main.MOBLIMA;
+import movie.Movie;
 
 public class CustomerController {
 	private void displayCustomerMenu() {
 		System.out.println(
 				"======================= Customer Menu =======================\n" + 
 				"1) List all available movies\n" + 
-				"2) View single movie details\n" +
-				"3) Make a booking\n" +
-				"4) View booking history\n" +
-				"5) List top 5 ranking movies\n" +
-				"6) Exit");
+				"2) List top 5 ranking movies\n" +
+				"3) View single movie details\n" +
+				"4) Leave a movie review\n" +
+				"5) Make a booking\n" +
+				"6) View booking history\n" +
+				"7) Exit");
 		System.out.print("Please select an option: ");
 	}
 	
@@ -20,23 +24,32 @@ public class CustomerController {
 		do {
 			displayCustomerMenu();
 			
-			switch (IntegerHandler.readInt(1, 6)) {
+			switch (IntegerHandler.readInt(1, 7)) {
 			case 1:
-				break;
-
-			case 2:
+				MOBLIMA.movieController.displayAllAvailableMovies();
 				break;
 				
+			case 2:
+				System.out.print("Sort by ticket sales? (Y/N) ");
+				MOBLIMA.movieController.listTopFive(StringHandler.readString("Y", "N").equals("Y"));
+				break;
+
 			case 3:
+				Movie movie = MOBLIMA.movieController.selectMovie(false);
+				movie.displayFullDetails();
 				break;
 				
 			case 4:
+				MOBLIMA.reviewController.writeReview();
 				break;
 				
 			case 5:
 				break;
 				
 			case 6:
+				break;
+				
+			case 7:
 				System.out.println("Exiting Admin Application ...");
 				done = true;
 			}
