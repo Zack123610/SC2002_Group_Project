c41f667a64c05cc9bb8bbda0366556c722ffb906
Sele3 <STAN266@e.ntu.edu.sg>
2022-10-31 22:40:22 +0800

    edit review attribute
    Review no longer needs to store the movie as its attribute

--- src/movie/review/Review.java
+++ src/movie/review/Review.java
@@ -9,16 +9,20 @@ public class Review extends Writable {
 	 * 
 	 */
 	private static final long serialVersionUID = 5785020867407651793L;
-	private Movie movie;
+	//private Movie movie;
 	private int rating;
 	private String description;
 	
 	public Review() { }
-	public Review(Movie movie, int rating, String description) {
-		this.movie = movie;
+	public Review(int rating, String description) {
 		this.rating = rating;
 		this.description = description;
 	}
+//	public Review(Movie movie, int rating, String description) {
+//		this.movie = movie;
+//		this.rating = rating;
+//		this.description = description;
+//	}
 	
 	
 	@Override
@@ -26,11 +30,11 @@ public class Review extends Writable {
 		return String.format("%d★\n%s", rating, description);
 	}
 	
-	public Movie getMovie() { return movie; }
+	//public Movie getMovie() { return movie; }
 	public String getDescription() { return description; }
 	public int getRating() { return rating; }
 	
-	public void setMovie(Movie movie) { this.movie = movie; }
+	//public void setMovie(Movie movie) { this.movie = movie; }
 	public void setDescription(String description) { this.description = description; }
 	public void setRating(int rating) { this.rating = rating; }
 }
--- src/movie/review/ReviewController.java
+++ src/movie/review/ReviewController.java
@@ -6,30 +6,33 @@ import java.util.Map;
 import java.util.UUID;
 
 import input.FileController;
+import input.IntegerHandler;
+import input.StringHandler;
 import main.MOBLIMA;
+import movie.Movie;
 
 public class ReviewController {
-	private List<Review> reviews;
-	private Map<UUID, Review> hm = new HashMap<>();
+//	private List<Review> reviews;
+//	private Map<UUID, Review> hm = new HashMap<>();
 	
 	// Initialisation Code
 	public ReviewController() {
-		reviews = FileController.read("./data/review/");
+//		reviews = FileController.read("./data/review/");
 	}
 	public void init() {
-		for (Review review : reviews) 
-			review.setMovie(MOBLIMA.movieController.getMovieByID(review.getMovie().getID()));
+//		for (Review review : reviews) 
+//			review.setMovie(MOBLIMA.movieController.getMovieByID(review.getMovie().getID()));
 		System.out.println("Review Controller initialised successfully!");
 	}
 	public void exit() {
-		FileController.write(reviews, "./data/review/");
+//		FileController.write(reviews, "./data/review/");
 		System.out.println("Review Controller exited successfully!");
 	}
-	public Review getReviewByID(UUID id) {
-		return hm.containsKey(id) ? hm.get(id) : null;
-	}
+//	public Review getReviewByID(UUID id) {
+//		return hm.containsKey(id) ? hm.get(id) : null;
+//	}
 	
-	
+
 	
 	
 	
