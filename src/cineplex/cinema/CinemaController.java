6750ee30edcfd05fe145664bea0fff69f8224583
Zack123610 <104198472+Zack123610@users.noreply.github.com>
2022-11-01 00:31:24 +0800

    Updated CinemaController

--- src/cineplex/cinema/CinemaController.java
+++ src/cineplex/cinema/CinemaController.java
@@ -4,9 +4,27 @@ public class CinemaController {
 	// Cinema controller no longer needs to preload any data 
 	// (The cinema objects are either stored within showtime or cineplex)
 	// It now only needs to handle the seat booking when passed in Cinema object as parameter
-	public CinemaController() {	}
+	public CinemaController() {	
+		/*
+		// display seating layout
+		public void displaySeating(){
+			cinema.displaySeatingLayout();
+		}
 
-	
-	
-	
+		// book seat
+		public Seat cinemaBookSeat(char row, int col){
+			if (cinema.isFull()){
+				System.out.println("Error. No more seats available! Try another Cinema!")
+			} else {
+				return cinema.bookSeat(row, col);
+			}
+		}
+
+		// clear seat
+		public Seat cinemaClearSeat(char row, int col){
+			return cinema.clearSeat(row, col);
+		}
+		*/
+	}
+
 }
