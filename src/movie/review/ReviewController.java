d2561f454ff1df2c40c6cb3cee80ee5a0d8e02fa
Shan <44370959+Shanzzzzz@users.noreply.github.com>
2022-11-01 22:30:46 +0800

    Added methods for ReviewController

--- src/movie/review/ReviewController.java
+++ src/movie/review/ReviewController.java
@@ -1,12 +1,16 @@
 package movie.review;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import input.FileController;
+import input.IntegerHandler;
+import input.StringHandler;
 import main.MOBLIMA;
+import movie.Movie;
 
 public class ReviewController {
 	private List<Review> reviews;
@@ -29,10 +33,60 @@ public class ReviewController {
 		return hm.containsKey(id) ? hm.get(id) : null;
 	}
 	
-	
-	
-	
-	
+	// Creates a new review
+	public void createReview(){
+		Movie movie = null;
+		// Calls movie controller to help select a movie
+
+		//ArrayList<Movie> movieList = MOBLIMA.movieController.displayMovies();
+		//movie = MOBLIMA.movieController.selectMovie(movieList);
+
+		// Request user input rating
+		System.out.println("Rate the movie! [1-5 stars]");
+		int rating = IntegerHandler.readInt(1, 5);
+
+		// Request user input description
+		System.out.println("What do you think of the movie?");
+		String description = StringHandler.readString();
+
+		// Create Review object
+		Review r = new Review(movie, rating, description);
+
+		// Add Review object into reviews
+		reviews.add(r);
+	}
+
+	public double calculateRating(List<Review> reviews){
+		double rating = 0;
+
+		for(Review r : reviews){
+			rating += r.getRating();
+		}
+
+		return rating/reviews.size();
+	}
+
+	// Displays a list of reviews associated with the movie specified
+	public void getReviewsByMovie(Movie movie){
+		// filter reviews by movie
+		ArrayList<Review> r = new ArrayList<Review>(); 
+		for(Review review : reviews){
+			if(review.getMovie() == movie){
+				r.add(review);
+			}
+		}
+
+		// display reviews
+		if(r.size() == 0){ 
+			System.out.println("There are no reviews for this movie!");
+		}
+		else{
+			System.out.println("Overall rating: " + (r.size() == 1 ? "NA" : String.format("%.1f",calculateRating(r))));
+			for(Review review: r){
+				System.out.print(review);
+			}
+		}	
+	}
 	
 	
 	
