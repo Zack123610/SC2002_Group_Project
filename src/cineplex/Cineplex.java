ab5d6d8e023ee842d866db7970177e5495a440d4
Sele3 <STAN266@e.ntu.edu.sg>
2022-11-04 16:28:28 +0800

    add abstract cinema class

--- src/cineplex/Cineplex.java
+++ src/cineplex/Cineplex.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import cineplex.cinema.AbstractCinema;
 import cineplex.cinema.Cinema;
 import globals.Writable;
 import movie.showtime.Showtime;
@@ -17,7 +18,7 @@ public class Cineplex extends Writable {
 	private String name;
 	private List<UUID> showTimeIDList; // For showTime IDs
 	private List<Showtime> showTimes; // ArrayList for showTime
-	private List<Cinema> cinemaList;  // ArrayList for cinemas
+	private List<AbstractCinema> cinemaList;  // ArrayList for cinemas
 	
 	// Constructor
 	public Cineplex() {}
@@ -32,13 +33,13 @@ public class Cineplex extends Writable {
 	public String getName() { return name; }
 	public List<UUID> getShowTimeIDList() { return showTimeIDList; }
 	public List<Showtime> getShowTimes() { return showTimes; }
-	public List<Cinema> getCinemaList() { return cinemaList; }
+	public List<AbstractCinema> getCinemaList() { return cinemaList; }
 	
 	public void setShowTimes(List<Showtime> showTimes) { this.showTimes = showTimes; }
-	public void setCinemaList(List<Cinema> cinemaList) { this.cinemaList = cinemaList; }
+	public void setCinemaList(List<AbstractCinema> cinemaList) { this.cinemaList = cinemaList; }
 	
 	public void addShowTime(Showtime showTime) { showTimes.add(showTime); }
-	public void addCinema(Cinema cinema) {
+	public void addCinema(AbstractCinema cinema) {
 		cinemaList.add(cinema);
 	}
 }
